6f69a621abc930f1da6d44d000d72b3bea4b3177
Dan <danoosh.tabatabaei11@gmail.com>
2023-12-19 16:33:31 +0330

    My first js code

---
+++ 01/session01.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+x = "Danoosh";
+y = "Tabatabaei";
+console.log("My name is ", x, "and my last name is ", y);
+let age = 21;
+let height = 190;
+console.log("I'm ", age, "years old!")
+console.log("I'm ", height, "cm tall!")
+
+console.log("Type of Age is ", typeof age, "and value of age is", age)
+console.log("Value of x is ", typeof x, "and value of x is", x)
+z = [1, 2, "three", "Four"];
